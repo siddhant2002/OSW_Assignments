756837c6f1c1c4e5ea8ba64a20c683c02cd90e48
Siddhant2002 <siddhantswarupmallick@gmail.com>
2022-12-30 11:44:14 +0530

    Assignment 3 updated

--- Day 4/Num1.c
+++ Day 4/Num1.c
@@ -9,3 +9,56 @@ int main(int argc, char *argv[])
     }
     return 0;
 }
+Outputs
+a -> 1
+
+b -> 4
+ITER
+SOA
+IBCS
+
+c -> 2
+12 34 56
+
+d -> 7
+ITER
+SOA
+IBCS
+"23
+45
+67"
+
+e -> 3
+./a.out
+123456
+
+f -> 3
+./a.out
+'123456'
+
+g -> 3
+./a.out
+"123456"
+
+h -> 6
+./a.out
+12
+14
+45
+66
+
+i -> 2
+12345678
+
+j -> 1
+
+k -> No of arguments are 4
+argv[0]: ./a.out
+argv[1]: 12
+argv[2]: /home/2041013040
+argv[3]: 34
+
+l -> 4
+12
+'pwd'
+34
